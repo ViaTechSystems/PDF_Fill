af053d91903ec1758c319d82f01e7a2d91cb2c9d
Matt H <mhoskins@viatechsystems.com>
2019-03-16 11:13:25 -0400

    PyPDF2 update

--- untitled/views.py
+++ untitled/views.py
@@ -1,12 +1,29 @@
 from io import BytesIO
 
+import PyPDF2
 from django.http import HttpResponse
-import pdfrw
-from django.shortcuts import render
+
+from PyPDF2.generic import BooleanObject, NameObject, IndirectObject
 
 
 def pdf(request):
     template = 'templates/template.pdf'
+
+    outfile = "templates/test.pdf"
+
+    input_stream = open(template, "rb")
+    pdf_reader = PyPDF2.PdfFileReader(input_stream, strict=False)
+    if "/AcroForm" in pdf_reader.trailer["/Root"]:
+        pdf_reader.trailer["/Root"]["/AcroForm"].update(
+            {NameObject("/NeedAppearances"): BooleanObject(True)})
+
+    pdf_writer = PyPDF2.PdfFileWriter()
+    set_need_appearances_writer(pdf_writer)
+    if "/AcroForm" in pdf_writer._root_object:
+        # Acro form is form field, set needs appearances to fix printing issues
+        pdf_writer._root_object["/AcroForm"].update(
+            {NameObject("/NeedAppearances"): BooleanObject(True)})
+
     data_dict = {
         'first_name': 'John\n',
         'last_name': 'Smith\n',
@@ -23,10 +40,19 @@ def pdf(request):
 
     }
 
-    print(fill_in_pdf(template, data_dict).getvalue())
-    #
-    response = HttpResponse(fill_in_pdf(template, data_dict).getvalue(), content_type='application/pdf')
+    pdf_writer.addPage(pdf_reader.getPage(0))
+    pdf_writer.updatePageFormFieldValues(pdf_writer.getPage(0), data_dict)
+
+    output_stream = BytesIO()
+    pdf_writer.write(output_stream)
+
+    # print(fill_in_pdf(template, data_dict).getvalue())
+
+    # fill_in_pdf(template, data_dict).getvalue()
+    response = HttpResponse(output_stream.getvalue(), content_type='application/pdf')
     response['Content-Disposition'] = 'inline; filename="completed.pdf"'
+    input_stream.close()
+
     return response
     # return render(request,
     #               'index.html',
@@ -34,36 +60,20 @@ def pdf(request):
     #               )
 
 
-def fill_in_pdf(input_pdf_path, data_dict):
-    ANNOT_KEY = '/Annots'
-    ANNOT_FIELD_KEY = '/T'
-    ANNOT_VAL_KEY = '/V'
-    ANNOT_RECT_KEY = '/Rect'
-    SUBTYPE_KEY = '/Subtype'
-    WIDGET_SUBTYPE_KEY = '/Widget'
+def set_need_appearances_writer(writer):
+    # See 12.7.2 and 7.7.2 for more information:
+    # http://www.adobe.com/content/dam/acom/en/devnet/acrobat/pdfs/PDF32000_2008.pdf
+    try:
+        catalog = writer._root_object
+        # get the AcroForm tree and add "/NeedAppearances attribute
+        if "/AcroForm" not in catalog:
+            writer._root_object.update({
+                NameObject("/AcroForm"): IndirectObject(len(writer._objects), 0, writer)})
 
-    template_pdf = pdfrw.PdfReader(input_pdf_path)
-    annotations = template_pdf.pages[0][ANNOT_KEY]
-    for annotation in annotations:
-        # print(annotation)
-        if annotation[SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
-            if annotation[ANNOT_FIELD_KEY]:
-                key = annotation[ANNOT_FIELD_KEY][1:-1]
-                # print(key)
-                if key in data_dict.keys():
-                    # annotation['/V'] = annotation[ANNOT_FIELD_KEY]
-                    # annotation[SUBTYPE_KEY] = '/T'
-                    annotation.update(
-                        pdfrw.PdfDict(V='{}'.format(data_dict[key], ))
-                    )
+        need_appearances = NameObject("/NeedAppearances")
+        writer._root_object["/AcroForm"][need_appearances] = BooleanObject(True)
+        return writer
 
-
-    # does not work or it would show in view.
-    memory_pdf = BytesIO()
-    pdfrw.PdfWriter().write(memory_pdf, template_pdf)
-
-    # pdfrw.PdfWriter().write('templates/output.pdf', template_pdf)
-
-    print(str(memory_pdf.getvalue()).replace('/Widget', ''))
-
-    return memory_pdf
+    except Exception as e:
+        print('set_need_appearances_writer() catch : ', repr(e))
+        return writer
